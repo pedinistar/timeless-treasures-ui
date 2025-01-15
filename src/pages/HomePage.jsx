5877dbc8c9e65c97857aedfd2349cf89d04c10d5
Jayashree <pedinijayashree03@gmail.com>
2025-01-15 23:19:11 +0530

    NavBar

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <main className="">
+      <h1 className="text-light">Home Page</h1>
+    </main>
+  );
+};
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4E403C",
+        secondary: "#997C70",
+        light: "#F0F0D7",
+      },
+      fontFamily: {
+        primary: ["Cinzel Decorative", "serif"],
+        secondary: ["Tangerine", "serif"],
+        base: ["Times New Roman", "serif"],
+      },
+    },
   },
   plugins: [],
 };
